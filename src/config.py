869a3d55c75b2bad3bb1a57b13d2bcf5a6fe9aa4
omodunnialice-blip <omodunnialice30@.com>
2026-07-29 09:21:22 +0100

    test: temporarily add broken target for demo

--- src/config.py
+++ src/config.py
@@ -2,4 +2,4 @@ TARGETS = [
     {"name": "Main Website", "url": "https://example.com", "timeout": 5},
     {"name": "Broken Test Site", "url": "https://this-site-does-not-exist-xyz123.com", "timeout": 5},
 ]
-FAILURE_THRESHOLD = 2
+FAILURE_THRESHOLD = 2s
